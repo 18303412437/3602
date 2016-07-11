15490dd97753a99b5bdbcede4fcd6323f19bf75c
18303412437 <812159464@qq.com>
2016-07-11 09:05:39 +0800

    360

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+});
